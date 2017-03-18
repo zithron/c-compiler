8726bd7fcbec94449fe622dc205b6d51e441dd02
zithron <abdulbasithb70@gmail.com>
2017-03-18 22:10:23 +0700

    halaman deskripsi tiap lomba

--- js/init.js
+++ js/init.js
@@ -2,8 +2,8 @@
     $(function () {
 
         $('.button-collapse').sideNav();
-        $('.modal').modal();
         $('ul.tabs').tabs();
+       $('select').material_select();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
